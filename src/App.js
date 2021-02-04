be37d31432638910d790b4c73f3d154dfdb3c038
rkasza <robert.kasza@gremonsystems.com>
2021-02-04 16:49:35 +0100

    Create HelloWorldWithPureComponent component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import HelloWorldWithCreateElement from './components/HelloWorldWithCreateElement';
 import HelloWorldWithClass from './components/HelloWorldWithClass';
+import HelloWorldWithPureComponent from './components/HelloWorldWithPureComponent';
 
 function App() {
   return <>
     <HelloWorldWithCreateElement el='h2'>Hello World</HelloWorldWithCreateElement>
     <HelloWorldWithClass style={{ textAlign: 'center' }}>Hello World2</HelloWorldWithClass>
+    <HelloWorldWithPureComponent style={{ textAlign: 'right' }}>Hello World3</HelloWorldWithPureComponent>
   </>;
 }
 

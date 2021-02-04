416b5ea7ad2ec9833bf18262ce21da1ed6db929e
rkasza <robert.kasza@gremonsystems.com>
2021-02-04 16:42:31 +0100

    Create HelloWorldWithClass component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import HelloWorldWithCreateElement from './components/HelloWorldWithCreateElement';
+import HelloWorldWithClass from './components/HelloWorldWithClass';
 
 function App() {
   return <>
     <HelloWorldWithCreateElement el='h2'>Hello World</HelloWorldWithCreateElement>
+    <HelloWorldWithClass style={{ textAlign: 'center' }}>Hello World2</HelloWorldWithClass>
   </>;
 }
 

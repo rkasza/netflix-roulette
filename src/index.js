dd5e87182721f62866c738df80d4854a9915e07b
rkasza <robert.kasza@gremonsystems.com>
2021-02-07 17:31:23 +0100

    Setup webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Img from './notfound.png';
+import './style.css';
 
 const App = () => (
   <div>
-    <h1>Hello World</h1>
+    <h1 className="red">Hello Worl222dsdasdasdas</h1>
+    <img src={Img} />
   </div>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const HtmlPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  output: {
+    filename: 'bundle.js'
+  },
   module: {
     rules:[
       {
@@ -19,14 +24,30 @@ module.exports = {
             loader: 'html-loader'
           }
         ]
+      },
+      {
+        test :/\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       }
     ]
   },
-
   plugins: [
     new HtmlPlugin({
       filename: 'index.html',
       template: './src/index.html'
-    })
-  ]
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  devServer: {
+    historyApiFallback: true,
+    port: 5000,
+  }
 }

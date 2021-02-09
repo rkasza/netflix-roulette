3bc74f999c7ee835d96d0ac8cfe32fd4e0b2a12c
rkasza <robert.kasza@gremonsystems.com>
2021-02-09 16:59:47 +0100

    Setup file structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import HelloWorldWithCreateElement from './components/HelloWorldWithCreateElement';
-import HelloWorldWithClass from './components/HelloWorldWithClass';
-import HelloWorldWithPureComponent from './components/HelloWorldWithPureComponent';
-import HelloWorld from './components/HelloWorld';
-
+import './assets/css/index.css';
 
 function App() {
-  return <>
-    <HelloWorldWithCreateElement el='h2'>Hello World</HelloWorldWithCreateElement>
-    <HelloWorldWithClass style={{ textAlign: 'center' }}>Hello World2</HelloWorldWithClass>
-    <HelloWorldWithPureComponent style={{ textAlign: 'right' }}>Hello World3</HelloWorldWithPureComponent>
-    <HelloWorld style={{ textAlign: 'left', color: 'red' }}>Hello World4</HelloWorld>
-  </>;
+  return <div><span>netflixroulette</span></div>;
 }
 
 export default App;

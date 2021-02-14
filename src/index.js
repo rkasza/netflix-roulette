d5fb5c1d3df68a19e0e96952d3220ffd8661bf36
rkasza <robert.kasza@gremonsystems.com>
2021-02-14 13:54:34 +0100

    Import Skeleton css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './assets/css/skeleton-css/css/normalize.css';
+import './assets/css/skeleton-css/css/skeleton.css';
+import './assets/css/skeleton-css/css/custom.css';
 
 ReactDOM.render(
   <React.StrictMode>

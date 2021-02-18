25ea0d6f9eac2eab86529f1f65ff0d1a0096028f
rkasza <robert.kasza@gremonsystems.com>
2021-02-18 17:06:41 +0100

    Add menubutton to movie

--- src/pages/Home/Movies/Movie/Movie.js
+++ src/pages/Home/Movies/Movie/Movie.js
@@ -2,23 +2,43 @@ import React from 'react';
 import GenreList from './GenreList';
 import ReleaseYear from './ReleaseYear';
 import Image from '../../../../components/Image';
+import DotsVerticalIcon from 'mdi-react/DotsVerticalIcon'
 
-const Movie = props => {
-  const { title, poster_path, release_date, genres } = props;
-  return (
-    <div className="Movie">
-      <div className="Poster">
-        <Image src={poster_path}  alt={title} />
+class Movie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenuButton: false
+    }
+  }
+
+  handleOnMouseEnter () {
+    this.setState({ showMenuButton: true });
+  }
+
+  handleOnMouseLeave () {
+    this.setState({ showMenuButton: false });
+  }
+  render() {
+    const { title, poster_path, release_date, genres } = this.props;
+    const { showMenuButton } = this.state;
+    return (
+      <div className="Movie" onMouseEnter={this.handleOnMouseEnter.bind(this)} onMouseLeave={this.handleOnMouseLeave.bind(this)} >
+        {showMenuButton && <DotsVerticalIcon  className="MovieMenuButton" size={40}/>}
+        <div className="Poster">
+          <Image src={poster_path}  alt={title} />
+        </div>
+        <div className="Info">
+            <h5>
+              {title}
+              <ReleaseYear>{release_date.slice(0, 4)}</ReleaseYear>
+            </h5>
+            <GenreList genres={genres} />
+        </div>
       </div>
-      <div className="Info">
-          <h5>
-            {title}
-            <ReleaseYear>{release_date.slice(0, 4)}</ReleaseYear>
-          </h5>
-          <GenreList genres={genres} />
-      </div>
-    </div>
-  )
+    )
+  }
+  
 };
 
 export default Movie;

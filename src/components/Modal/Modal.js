84fd084764ff746e9be108ffa9332917c0796757
rkasza <robert.kasza@gremonsystems.com>
2021-03-04 16:37:59 +0100

    remove overlay click from modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,7 +4,7 @@ import CloseIcon from 'mdi-react/CloseIcon';
 import './Modal.css';
 
 const Modal = ({ children, className = '', show, onClose }) => (
-  <ModalWrapper className={className} show={show} onClose={onClose}>
+  <ModalWrapper className={className} show={show}>
     <div className="ModalContent">
       <CloseIcon className="Close" size={36} onClick={onClose}/>
       {children}
--- src/components/Modal/ModalWrapper.js
+++ src/components/Modal/ModalWrapper.js
@@ -6,8 +6,6 @@ class ModalWrapper extends React.Component {
     super(props);
     this.el = document.createElement('div');
     this.el.className = `Modal ${this.props.className}`;
-    this.handleOverLayClick = this.handleOverLayClick.bind(this);
-    this.el.addEventListener('click', this.handleOverLayClick);
   }
   componentDidMount() {
     document.body.appendChild(this.el);
@@ -15,15 +13,10 @@ class ModalWrapper extends React.Component {
   }
 
   componentWillUnmount() {
-      this.el.removeEventListener('click', this.onClose);
       document.body.removeChild(this.el);
       document.body.classList.remove('ModalActive');
   }
-  handleOverLayClick (event) {
-    if (event.target === this.el) {
-      this.props.onClose();
-    }
-  }
+
   render () {
     return ReactDOM.createPortal(
       this.props.children,

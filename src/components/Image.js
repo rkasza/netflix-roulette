0dc8e1db96bf4c49a8aacd9e8b0e037c0c7f5937
rkasza <robert.kasza@gremonsystems.com>
2021-03-25 14:46:44 +0100

    Use variable instead of ref in Image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,9 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types';
 
  const Image = props => {
-  const fallback = useRef('images/placeholder-movieimage.png');
-  const handleOnError = event => event.target.src = fallback.current;
+  const fallback = 'images/placeholder-movieimage.png';
+  const handleOnError = event => event.target.src = fallback;
   return <img {...props} onError={handleOnError} />; //eslint-disable-line
 }
 Image.propTypes = {

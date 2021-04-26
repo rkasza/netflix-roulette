5788cf5ec36138343b19feb4919d32fd35290401
rkasza <rbert.kasza@gmail.com>
2021-04-26 09:30:49 +0200

    Add classname to Logo props

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 import './Logo.css';
 
-const Logo = ({ fontSize = '20px' }) => <span className="Logo" style={{ fontSize }}><span className="BoldNetflix">netflix</span>roulette</span>
+const Logo = ({ fontSize = '20px', className = 'Logo' }) => <span className={className} style={{ fontSize }}><span className="BoldNetflix">netflix</span>roulette</span>
 
 Logo.propTypes = {
-  fontSize: PropTypes.string
+  fontSize: PropTypes.string,
+  className: PropTypes.string
 };
 
 export default Logo

b5e533aebd205cb9a4eaf7de9a475b3cc750633c
rkasza <rbert.kasza@gmail.com>
2021-04-26 15:25:32 +0200

    Use router to show MovieDetails component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ErrorBoundary from './components/ErrorBoundary';
-import Header from './parts/Header';
 import Logo from './components/Logo/Logo';
 import Container from  './components/Skeleton/Container';
 import Movies from './parts/Movies/Movies';
 import './assets/css/App.css';
 import Modal from './components/Modal/Modal';
 import useModal from './hooks/useModal';
+import FindMovie from './pages/Home/FindMovie';
+import MovieDetails from './pages/MovieDetails/MovieDetails';
 
 function App() {
   const { modalState: { show, modalBody, modalClassName },  closeModal } = useModal();
   
   return (
     <ErrorBoundary>
-      <Container className="PageContent">
-        <Header />
-        <Movies />
-        <footer><Logo fontSize='18px' /></footer>
-      </Container>
+      <Router>
+        <Container className="PageContent">
+          <Switch>
+            <Route path="/" exact><FindMovie /><Movies /></Route>
+            <Route path="/movie/:movieId"><MovieDetails /><Movies /></Route>
+          </Switch>
+          <footer><Logo fontSize='18px' /></footer>
+        </Container>
+      </Router>
       {show && <Modal onClose={closeModal} className={modalClassName}>{modalBody}</Modal>}
     </ErrorBoundary>
   );
--- src/pages/Home/FindMovie.js
+++ src/pages/Home/FindMovie.js
@@ -3,7 +3,7 @@ import Row from '../../components/Skeleton/Row';
 import Col from '../../components/Skeleton/Col';
 import HeroImage from '../../parts/HeroImage/HeroImage';
 import Logo from '../../components/Logo/Logo';
-import MovieFilter from './MovieFilter';
+import MovieFilter from './FindMovieForm';
 import MovieForm from '../../parts/MovieForm/MovieForm';
 import useModal from '../../hooks/useModal';
 import { createMovie } from '../../store/actions/movieActions';
@@ -14,26 +14,24 @@ const Home = () => {
   const dispatch = useDispatch();
   const handleOnSubmit = newMovie => {
     dispatch(createMovie(newMovie));
-  }
+  };
   const handleOnClick = () => {
     const modalBody = <MovieForm formTitle="ADD MOVIE" onSubmit={handleOnSubmit} />;
     openModal(modalBody, 'MovieFormModal');
   };
 
   return (
-    <>
-      <Row className="FindMovieWrapper">
-        <Col size={12}>
-          <HeroImage image="/images/movie-montage.jpg">
-              <div className="header">
-                <Logo />
-                <button className="button AddMovie" type="button" onClick={handleOnClick}>+ ADD MOVIE</button>
-              </div>
-              <MovieFilter />
-          </HeroImage>
-        </Col>
-      </Row>
-    </>
+    <Row className="FindMovieWrapper">
+      <Col size={12}>
+        <HeroImage image="/images/movie-montage.jpg">
+          <div className="header">
+            <Logo />
+            <button className="button AddMovie" type="button" onClick={handleOnClick}>+ ADD MOVIE</button>
+          </div>
+          <MovieFilter />
+        </HeroImage>
+      </Col>
+    </Row>
   );
 };
 

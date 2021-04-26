53aab16d158e6681082b0a14089eb69f1fa8044e
rkasza <rbert.kasza@gmail.com>
2021-04-26 16:21:58 +0200

    Add Link to Logo Component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,12 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import './Logo.css';
 
-const Logo = ({ fontSize = '20px', className = 'Logo' }) => <span className={className} style={{ fontSize }}><span className="BoldNetflix">netflix</span>roulette</span>
+const Logo = ({ fontSize = '20px', className = 'Logo' }) => (
+  <Link to="/" className={className} style={{ fontSize }}>
+    <span className="BoldNetflix">netflix</span>roulette
+  </Link>
+)
 
 Logo.propTypes = {
   fontSize: PropTypes.string,
